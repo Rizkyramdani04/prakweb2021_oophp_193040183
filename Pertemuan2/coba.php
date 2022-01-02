6168cedc23e23166eda76ca640c37eb8ab8b4b21
Rizkyramdani <rizkyramdani.rr05@gmail.com>
2022-01-02 14:12:20 +0700

    Menyelesaikan video class dan objek pertemuan2

---
+++ Pertemuan2/coba.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Rizky Ramdani
+193040183
+https://github.com/Rizkyramdani04/prakweb2021_oophp_193040183
+Pertemuan 1 - 1 Januari 2022
+Membuat Urel oophp
+*/
+?>
+
+<?php
+
+class Coba {
+
+}
+
+$a = new Coba();
+$b = new Coba();
+$c - new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
