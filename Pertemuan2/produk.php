d9c4069a1e2b3b6ab56ce876d17122c313a47338
Rizkyramdani <rizkyramdani.rr05@gmail.com>
2022-01-02 14:13:39 +0700

    Menyelesaikan video class dan objek pertemuan2

---
+++ Pertemuan2/produk.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Rizky Ramdani
+193040183
+https://github.com/Rizkyramdani04/prakweb2021_oophp_193040183
+Pertemuan 1 - 1 Januari 2022
+Membuat Urel oophp
+*/
+?>
+
+<?php
+
+class Produk {
+
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
