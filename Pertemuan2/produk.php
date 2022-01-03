87aea4ce6e960f2357f80c9b601823b59f7b3a87
Rizkyramdani <rizkyramdani.rr05@gmail.com>
2022-01-04 05:48:11 +0700

    Menyelesaikan tugas video Property dab Method pertemuan 2

--- Pertemuan2/produk.php
+++ Pertemuan2/produk.php
@@ -1,22 +1,38 @@
 <?php
-/*
-Rizky Ramdani
-193040183
-https://github.com/Rizkyramdani04/prakweb2021_oophp_193040183
-Pertemuan 1 - 1 Januari 2022
-Membuat Urel oophp
-*/
-?>
-
-<?php
 
 class Produk {
-    public $judul,
-           $penulis,
-           $penerbit,
-           $harga;
+    public $judul = "judul",
+           $penulis = "penulis",
+           $penerbit = "penerbit",
+           $harga = 0;
+
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
 }
 
-$produk1 = new Produk();
-var_dump($produk1);
-?>
+
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Hello";
+// $produk2->tambahProperty = "Selamat Pagi";
+// var_dump($produk2->judul);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shone Jump";
+$produk3->harga = 30000;
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 35000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
